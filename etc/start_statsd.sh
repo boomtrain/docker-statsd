229f0b0e69f492c16e9eba30c32f167e9b1c0164
tensinj <taugustin@zetaglobal.com>
2019-11-10 19:57:50 +0530

    use instance hostname for statsd prefix

--- etc/start_statsd.sh
+++ etc/start_statsd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-instance_id=$(wget 169.254.169.254/latest/meta-data/instance-id -q -O -)
-echo "Using instance id: "
-echo $instance_id
-export INSTANCE_ID=$instance_id
-INSTANCE_ID=$instance_id node /usr/local/src/statsd/stats.js /etc/default/statsd.js
+instance_hostname=$(wget 169.254.169.254/latest/meta-data/hostname -q -O - | cut -d '.' -f1)
+echo "Using hostname: $instance_hostname"
+export HOST_NAME=$instance_hostname
+HOST_NAME=$instance_hostname node /usr/local/src/statsd/stats.js /etc/default/statsd.js
